f464da0675d5a90129cc5c6c3fa30369d9086b5a
SPccman <450760206@qq.com>
2021-03-29 23:31:02 +0800

    update cmake files

--- cmake/yama.cmake
+++ cmake/yama.cmake
@@ -3,6 +3,8 @@ if(NOT YAMA_CMAKE_ENTRY)
 
     set(YAMA_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
+    include (${CMAKE_CURRENT_LIST_DIR}/find_platform.cmake)
+
     # include cmake functions
     # CMAKE_CURRENT_LIST_DIR means the directory of this file
     # find all files in function directory, result will be return as relative path to CMAKE_CURRENT_LIST_DIR
